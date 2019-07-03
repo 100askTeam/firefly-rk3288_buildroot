11e31444010644d1b16a0ea090f9647a642db8b9
codebug8 <livelwh@outlook.com>
2019-07-03 05:52:58 -0400

    Update mjpg-streamer code
    Signed-off-by: codebug8 <livelwh@outlook.com>

---
+++ board/100ask/firefly-rk3288/rootfs-overlay/usr/camera/lcd/SourceCode/convert/.jdatasrc-tj.o.d
@@ -0,0 +1,43 @@
+jdatasrc-tj.o: jdatasrc-tj.c \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdc-predef.h \
+ /work/app_code/include/jinclude.h /work/app_code/include/jconfig.h \
+ /work/100ask-am335x_buildroot/output/host/opt/ext-toolchain/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stddef.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdlib.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/features.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/sys/cdefs.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/wordsize.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/gnu/stubs.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/gnu/stubs-hard.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/waitflags.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/waitstatus.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/endian.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/endian.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/byteswap.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/types.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/typesizes.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/byteswap-16.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/sys/types.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/time.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/sys/select.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/select.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/sigset.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/time.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/sys/sysmacros.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/pthreadtypes.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/alloca.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/stdlib-bsearch.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/stdlib-float.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdio.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/libio.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/_G_config.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/wchar.h \
+ /work/100ask-am335x_buildroot/output/host/opt/ext-toolchain/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdarg.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/stdio_lim.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/sys_errlist.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/stdio.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/string.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/xlocale.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/string.h \
+ /work/100ask-am335x_buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/bits/string2.h \
+ /work/app_code/include/jpeglib.h /work/app_code/include/jmorecfg.h \
+ /work/app_code/include/jerror.h
