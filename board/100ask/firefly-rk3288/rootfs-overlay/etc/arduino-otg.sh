adfe0810908dc7cabd9bed533b20333d7a9f6778
codebug8 <livelwh@outlook.com>
2019-07-30 03:04:14 -0400

    Update arduino config.
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- board/100ask/firefly-rk3288/rootfs-overlay/etc/arduino-otg.sh
+++ board/100ask/firefly-rk3288/rootfs-overlay/etc/arduino-otg.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 mkdir -p /arduino/arduino-100ask
-MYFILE=/arduino/arduino-100ask/am335x_app
+MYFILE=/arduino/arduino-100ask/rk3288_app
 while :
 do
-        find /arduino/ -name "am335x*" > /dev/null
+        find /arduino/ -name "rk3288*" > /dev/null
 
-if [ -f "/arduino/arduino-100ask/am335x_app" ]
+if [ -f "/arduino/arduino-100ask/rk3288_app" ]
 then
-        mv /arduino/arduino-100ask/am335x_app  /root
-        chmod u+x /root/am335x_app
+        mv /arduino/arduino-100ask/rk3288_app  /root
+        chmod u+x /root/rk3288_app
         echo -e "\033[32m Application copy succeeded ! \033[0m"
         cd /root
 fi
